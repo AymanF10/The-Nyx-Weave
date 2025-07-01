5f61ee71e49a9ff0599dc71c24be5de987b9b6d4
Swinecoder101 <liamfaruq@gmail.com>
2025-07-01 18:15:12 +0100

    clean

--- ts-backend/dist/services/price-quote.service.d.ts
+++
@@ -1,9 +0,0 @@
-import { PoolConfig, PriceQuote, RpcConfig } from "../types";
-export declare class PriceQuoteService {
-    private connection;
-    constructor(rpcConfig: RpcConfig);
-    getPriceQuote(poolConfig: PoolConfig): Promise<PriceQuote>;
-    private calculatePrice;
-    getPriceQuotesForAllPools(poolConfigs: PoolConfig[]): Promise<PriceQuote[]>;
-}
-//# sourceMappingURL=price-quote.service.d.ts.map
